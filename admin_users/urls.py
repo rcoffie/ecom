645d2dcaf25c50b83e2fbab11a6ae8fd227e59ca
Robert Coffie <rcoffie22@yahoo.com>
2022-06-25 20:59:14 +0000

    working on admin_users app

--- admin_users/urls.py
+++ admin_users/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
+from admin_users.views import AdminProductList, AdminProductDetail
 
 urlpatterns = [
-
-
-
+path('admin-list-product/',AdminProductList.as_view(), name='admin_list_product'),
+path('admin-product-detail/<str:pk>/', AdminProductDetail.as_view(), name='admin_product_detail'),
 ]
--- admin_users/views.py
+++ admin_users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from product.serializers import ProductSerializer, CartSerializer, CategorySerializer
+from product.models import Category, Cart, Product
+from rest_framework import generics
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser
 # Create your views here.
+
+
+class AdminProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
+
+
+class AdminProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
